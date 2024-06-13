30490614f1c387e05f0cc3a3cb71e62cd7095194
tung175 <duongkq3@gmail.com>
2024-06-13 16:48:02 +0700

    finish redux base

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,37 @@
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap"
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-    return (
-        <>
-        <Navbar bg="light" expand='lg'>
-            <Container>
-                <Navbar.Brand href="">Redux React</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="">Home</Nav.Link>
-                        <Nav.Link href="">Link</Nav.Link>
-                        <NavDropdown title='basic-nav-dropdown' id="basic-nav-dropdown">
-                            <NavDropdown.Item href="">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="">Action</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-        </>
-    )
-}
+  const listUsers = useSelector((state) => state.user.listUsers);
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="">Redux React</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <NavDropdown
+                className="ml-3"
+                title={`(${listUsers.length}) users`}
+                id="basic-nav-dropdown"
+              >
+                {listUsers &&
+                  listUsers.length > 0 &&
+                  listUsers.map((item, index) => {
+                    return (
+                      <NavDropdown.Item key={`count-users-${index}`}>
+                        {item.email}
+                      </NavDropdown.Item>
+                    );
+                  })}
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
-export default Header
+export default Header;
